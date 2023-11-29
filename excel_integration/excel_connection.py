5aec691b055ad66000c76d0e6fd0ea5931b159ed
jud-as <guilhermeferreira082@gmail.com>
2023-11-28 22:23:22 -0300

    correção de bugs

--- excel_integration/excel_connection.py
+++ excel_integration/excel_connection.py
@@ -18,16 +18,16 @@ class Bridge:
         engine = create_engine(self.string_de_conexao)
         consulta_sql = f'SELECT * FROM {tabela}'
         df_sql = pd.read_sql(consulta_sql, con=engine)
-        df_sql.to_excel(self.excel_path)
-        engine.dispose
+        df_sql.to_excel(self.excel_path, sheet_name='Gastos', index=False)
+        engine.dispose()
 
     def exportar_excel_para_sql(self, tabela):
         opc = 0
         engine = create_engine(self.string_de_conexao)
         df_excel = pd.read_excel(self.excel_path, sheet_name='Adicionar_Gastos') #Ler dados de um Excel para um dataframe do Pandas
         while True:
-            display(df_excel)
-            opc = input("\nDESEJA INSERIR ESSES DADOS?\n1 - Sim\n2 - Não")
+            print(df_excel)
+            opc = int(input("\nDESEJA INSERIR ESSES DADOS?\n1 - Sim\n2 - Não"))
             if opc == 1:
                 try:
                     df_excel.to_sql(tabela, con=engine, if_exists='append', index=False)
@@ -36,7 +36,8 @@ class Bridge:
                     print("NÃO FOI POSSÍVEL ALOCAR OS DADOS NA TABELA.")
                     break
             else:
-                break                           
+                break      
+        engine.dispose()                     
 
 
 
